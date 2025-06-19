a4b246f287eaf26ddb517d5cb7923b2bf3957578
ViSky <starovoytov2007@gmail.com>
2025-06-19 18:54:32 +0200

    chore(Player): понижена скорость всплытия и ускорение

--- Assets/Scripts/PlayerScripts/OnWaterEnteringTrigger.cs
+++ Assets/Scripts/PlayerScripts/OnWaterEnteringTrigger.cs
@@ -4,25 +4,23 @@ public class OnWaterEnteringTrigger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody rigidbody;
-    private SphereCollider collider;
     public float floatUpSpeedLimit = 1.30f;
     public float floatUpSpeed = 1.2f;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<SphereCollider>();
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.layer == 6)
         {
-            float difference = (other.transform.position.y - transform.position.y) * floatUpSpeed + 15f;
+            float difference = (other.transform.position.y - transform.position.y) * floatUpSpeed;
             rigidbody.AddForce(new Vector3(0f, Mathf.Clamp((Mathf.Abs(Physics.gravity.y) * difference), 0, Mathf.Abs(Physics.gravity.y) * floatUpSpeedLimit), 0f), ForceMode.Acceleration);
             rigidbody.linearDamping = 0.99f;
             rigidbody.angularDamping = 0.8f;
-            PlayerWalking._maxPlayerSpeed = 4f;
-            PlayerWalking._playerAccelaration = 0.1f;
+            PlayerWalking._maxPlayerSpeed = 2.4f;
+            PlayerWalking._playerAccelaration = 0.02f;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -31,8 +29,8 @@ public class OnWaterEnteringTrigger : MonoBehaviour
         {
             rigidbody.linearDamping = 0f;
             rigidbody.angularDamping = 0f;
-            PlayerWalking._maxPlayerSpeed = 8f;
-            PlayerWalking._playerAccelaration = 0.2f;
+            PlayerWalking._maxPlayerSpeed = 4f;
+            PlayerWalking._playerAccelaration = 0.1f;
         }
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerScripts/PlayerWalking.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class PlayerWalking : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] public static float _maxPlayerSpeed = 8f;
-    [SerializeField] public static float _playerAccelaration = 0.2f;
+    [SerializeField] public static float _maxPlayerSpeed = 4f;
+    [SerializeField] public static float _playerAccelaration = 0.1f;
     [SerializeField] private Joystick _joystick;
     private float _playerSpeed;
     private Vector3 _movementDirection;
@@ -17,12 +17,14 @@ public class PlayerWalking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #if UNITY_ANDROID
+#if UNITY_ANDROID
             _movementDirection = new Vector3(_joystick.Vertical * -1, 0, _joystick.Horizontal);
-        #else
-            _movementDirection = new Vector3(Input.GetAxis("Vertical") * -1, 0, Input.GetAxis("Horizontal"));
-        #endif
+#else
+
+        _movementDirection = new Vector3(Input.GetAxis("Vertical") * -1, 0, Input.GetAxis("Horizontal"));
+#endif
         _Rigidbody.transform.position += _movementDirection * _playerSpeed * Time.deltaTime;
+        
     }
 
     private void FixedUpdate()
